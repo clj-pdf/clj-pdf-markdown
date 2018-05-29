080fbf183f006c1a3c28b814a5756ea86394d10e
Leon Talbot <leontalbot@gmail.com>
2018-05-29 07:50:46 -0400

    bump to 0.2.1 (print removal)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pdf-markdown "0.2.0"
+(defproject clj-pdf-markdown "0.2.1"
   :description "Library for rendering markdown to clj-pdf data-structure syntax."
   :url "https://github.com/leontalbot/clj-pdf-markdown"
   :license {:name "Eclipse Public License"
